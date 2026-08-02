29b90e41ee7e693efb217774303b0f63e73b281f
lolipopede <kogo.go2712ko@outlook.de>
2026-08-02 22:26:45 +0200

    fixed Beta-Befehle

--- Code/assembler_try0.1.py
+++ Code/assembler_try0.1.py
@@ -1,24 +1,32 @@
 # CPU-Spezifikation
-OPCODES = {"AND(D,A)": 000000, "0": 000010, "OR(D,A)": 000100,
-            "XOR":001000, "NEG(D)": 001100,
-            "NEG(A)": 001101, "-1": 001110, "D + A": 010000,"D - A": 010100,
-            "A - D": 010101, "-A": 010110, "-D": 010111, "D + 1": 011000,
-            "A + 1": 011001, "1": 011010, "D - 1": 011100, "A - 1": 011101,
-            "AND(D,M)": 100000, "OR(D,M)": 100100, "XOR(D,M)": 101000,
-            "NEG(M)": 101101, "D + M": 110000, "D - M": 110100, "M - D": 110101,
-            "-M": 110110, "M + 1": 111001, "M - 1": 111101}
-Ziel = {"dw": 000, "wM": 001, "wD": 010, 
-        "wA": 100, "wDM": 011, "wAM": 101, 
-        "wAD": 110, "wADM": 111}
-Jump = {"no jump": 000, "comp>0": 001, "comp=0": 010, "comp>=0": 011,
-        "comp<0": 100, "comp/=0": 101, "comp=<0": 110, "comp always": 111}
-REGISTERS = {"A": 000110, "D": 000111, "M": 100110}
+OPCODES = {"AND(D,A)": 0b000000, "0": 0b000010, "OR(D,A)": 0b000100,
+            "XOR":0b001000, "NEG(D)": 0b001100,
+            "NEG(A)": 0b001101, "-1": 0b001110, "D+A": 0b010000,"D-A": 0b010100,
+            "A-D": 0b010101, "-A": 0b010110, "-D": 0b010111, "D+1": 0b011000,
+            "A+1": 0b011001, "1": 0b011010, "D-1": 0b011100, "A-1": 0b011101,
+            "AND(D,M)": 0b100000, "OR(D,M)": 0b100100, "XOR(D,M)": 0b101000,
+            "NEG(M)": 0b101101, "D+M": 0b110000, "D-M": 0b110100, "M-D": 0b110101,
+            "-M": 0b110110, "M+1": 0b111001, "M-1": 0b111101}
+Ziel = {"dw": 0b000, "wM": 0b001, "wD": 0b010, 
+        "wA": 0b100, "wDM": 0b011, "wAM": 0b101, 
+        "wAD": 0b110, "wADM": 0b111}
+Jump = {"nj": 0b000, "comp>0": 0b001, "comp=0": 0b010, "comp>=0": 0b011,
+        "comp<0": 0b100, "comp/=0": 0b101, "comp=<0": 0b110, "comp always": 0b111}
+REGISTERS = {"A": 0b000110, "D": 0b000111, "M": 0b100110}
 
 def translate_line(line):
     # Beispiel-Eingabe: "AND R1, R2"
-    parts = line.replace(",", "").split()
+    parts = line.replace(",", " ").split()
     cmd, direction, target = parts[0], parts[1], parts[2]
     
     # Bits zusammenbauen: Opcode (4 Bit) + Reg1 (2 Bit) + Reg2 (2 Bit)
-    instruction = ("0b" + OPCODES[cmd] + REGISTERS[direction] + REGISTERS[target])
-    return instruction # Liefert eine 16-Bit-Zahl
+    instruction = "0b1111" + format(OPCODES[cmd], "06b") + format(Ziel[direction], "03b") + format(Jump[target], "03b")
+    print(instruction)# Liefert eine 16-Bit-Zahl
+   
+
+
+with open(r"C:\Users\kogog\OneDrive\Maturarbeit\Code\test_for_assembler.txt", "r", encoding="utf-8") as datei:
+    inhalt = datei.read()
+    translated_lines = [translate_line(line) for line in inhalt.splitlines() if line.strip()]
+    datei.close()
+
